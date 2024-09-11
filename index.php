3e6967e607c3bb13abd25589a9565ae54d244537
brucordeiro <brunapcordeiro7@gmail.com>
2024-09-11 09:31:14 -0300

    Criado o estilo CSS

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="estillo.css" >
 </head>
 <body>
     <h1>Versionamento com GT/GITHUB </h1>
     <h2>Aula 11.09</h2>
     <p>Executando git commit<p>
+    
 </body>
 </html>
